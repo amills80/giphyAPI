a155f88b81b466eebf7f3117a0a5ea7c10de9867
Alan Mills <aelonrench@yahoo.com>
2018-03-27 12:03:05 -0400

    Create a default query upon loading. Implement a No-Results event. Convert search query as a ref.

--- src/App.js
+++ src/App.js
@@ -9,21 +9,13 @@ export default class App extends Component {
   constructor() {
     super();
     this.state = {
-      gifs: []
+      gifs: [],
+      loading: true
     };
   }
 
 componentDidMount() {
-// Summary: Transferring this function into performSearch()
-  // axios.get('http://api.giphy.com/v1/gifs/trending?api_key=dc6zaTOxFJmzC')
-  // .then(response => {
-  //   this.setState({
-  //     gifs: response.data.data
-  //   })
-  // })
-  // .catch(error => {
-  //   console.log('Error fetching and parsing data', error);
-  // });
+  this.performSearch();
 }
 
 // Summary: Perform above data fetch using the basic fetch method.
@@ -38,11 +30,12 @@ componentDidMount() {
   //       });
   // }
 
-  performSearch = (query) => {
+  performSearch = (query = 'cats') => {
     axios.get(`http://api.giphy.com/v1/gifs/search?q=${query}&limit=24&api_key=dc6zaTOxFJmzC`)
     .then(response => {
       this.setState({
-        gifs: response.data.data
+        gifs: response.data.data,
+        loading: false
       })
     })
     .catch(error => {
@@ -61,7 +54,9 @@ componentDidMount() {
           </div>
         </div>
         <div className="main-content">
-          <GifList data={this.state.gifs} />
+        {
+          (this.state.loading)?<p>Loading...</p> : <GifList data={this.state.gifs} />
+        }
         </div>
       </div>
     );
--- src/Components/GifList.js
+++ src/Components/GifList.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import Gif from './Gif';
+import NoGifs from './NoGifs';
 
 const GifList = props => {
   const result = props.data;
-  let gifs = result.map(gif =>
-    <Gif url={gif.images.fixed_height.url} key={gif.id}/>
-  );
+  let gifs;
+
+  if(result.length > 0)
+  {
+    gifs = result.map(gif => <Gif url={gif.images.fixed_height.url} key={gif.id}/>);
+  }
+  else
+  {
+    gifs = <NoGifs />
+  }
 
   return(
     <ul className="gif-list">
